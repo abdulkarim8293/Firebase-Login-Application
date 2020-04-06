ed37740bd0ded9bfdece3b2cef774f8146a15039
unknown <abdulkarim8293@gmail.com>
2020-04-07 03:56:25 +0600

    add splash activity

---
+++ app/src/main/java/com/abdulkarim/firebaseloginapp/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.abdulkarim.firebaseloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
