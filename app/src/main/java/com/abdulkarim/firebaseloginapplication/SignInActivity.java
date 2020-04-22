4eb85c7031fbc9410b376eca0c022e3cf6d3750a
unknown <abdulkarim8293@gmail.com>
2020-04-22 23:57:39 +0600

    add data binding

--- app/src/main/java/com/abdulkarim/firebaseloginapplication/SignInActivity.java
+++ app/src/main/java/com/abdulkarim/firebaseloginapplication/SignInActivity.java
@@ -1,14 +1,21 @@
 package com.abdulkarim.firebaseloginapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.abdulkarim.firebaseloginapp.databinding.ActivitySignInBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_in);
     }
 }
